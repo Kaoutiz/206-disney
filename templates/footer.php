9408a7758901a41edf93914812374ae6eca51900
dotonprincess <princess.doton@etu.unilim.fr>
2023-01-27 21:11:54 +0100

    footer base

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,117 @@
 <footer>
     <!-- Formulaire de contact + footer et plan du site (ne pas oublier l'adresse du parc) -->
+
+    <div class="container footer-menu container-fluid px-lg-5 pb-0">
+            <div class="mt-4">
+
+                <!-- Onglets -->
+                <ul class="nav nav-tabs nav-pills flex-column flex-sm-row text-center bg-light border-0 rounded-nav">
+                    <li role="presentation" class="nav-item flex-sm-fill">
+                        <a href="#contact-footer" class="nav-link border-0 text-uppercase font-weight-bold" data-bs-toggle="tab">Contact</a>
+                    </li>
+                    <li role="presentation" class="nav-item flex-sm-fill">
+                        <a href="#site-footer" class="nav-link border-0 text-uppercase font-weight-bold active" data-bs-toggle="tab">Plan du site</a>
+                    </li>
+                    <li role="presentation" class="nav-item flex-sm-fill">
+                        <a href="#infos-footer" class="nav-link border-0 text-uppercase font-weight-bold" data-bs-toggle="tab">Informations légales</a>
+                    </li>
+                    <li role="presentation" class="nav-item flex-sm-fill">
+                        <a href="#a-propos-footer" class="nav-link border-0 text-uppercase font-weight-bold" data-bs-toggle="tab">à propos</a>
+                    </li>
+                </ul>
+
+                <!-- Contenu des onglets -->
+                <div class="tab-content">
+                    <div id="contact-footer" class="tab-pane fade">
+                        <form>
+                            <div class="row g-5">
+                                 <div class="col-3 pt-3 infos-form">
+                                 <h3 class="text-uppercase font-weight-bold">Adresse du parc</h3>
+                                    <p class="mb-0">Disneyland Paris</p>
+                                    <p class="mb-0">Boulevard du Parc</p>
+                                    <p class="mb-0">Avenue Paul Seramy</p>
+                                    <p class="mb-0">77700 Serris | Magny le hongre</p>
+                                    <p class="mb-0">France</p>
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="Nom" aria-label="Nom" required>
+                                    <input type="text" class="form-control" placeholder="Prénom" aria-label="Prénom" required>
+                                    <input type="email" class="form-control" placeholder="E-mail" aria-label="E-mail" required>
+                                </div>
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="Sujet" aria-label="Sujet" required >
+                                    <textarea name="Message" type="text" class="form-control" placeholder="Votre message..." aria-label="Votre message..." required></textarea>
+                                    <button type="submit" class="btn btn-primary">Envoyer</button>
+                                </div>
+                            </div>
+                        </form>
+                        
+                    </div>
+                    <div id="site-footer" class="tab-pane fade show active">
+                        <div class="row row-cols-4 text-center">
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Accueil</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Actualités</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Nos Hôtels</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Offres</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Réservation</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Boutique</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Nos autres Parcs</a></div>
+                        </div>
+                    </div>
+                    <div id="infos-footer" class="tab-pane fade">
+                        <div class="row row-cols-4 text-center">
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Mentions légales et conditions d'utilisation du site web</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Règlement intérieur de nos Parcs</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Règlement intérieur de nos Hôtels</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Règlement intérieur du Disney Village</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Règles européennes de respect de la vie privée</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Utilisation des cookies</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Modifier vos préférences en matière de cookies</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Copyrights</a></div>
+                        </div>
+                    </div>
+                    <div id="a-propos-footer" class="tab-pane fade">
+                    <div class="row row-cols-4 text-center">
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Application mobile</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Affiliation</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Partenaires</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Presse</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Informations sur l'entreprise</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Offres d'emploi</a></div>
+                            <div class="col"><a href="#" class="nav-link m-2 font-weight-bold" data-bs-toggle="tab">Abonnement DisneyPass</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Réseaux & Adresse-->
+            <hr/>
+            <div class="row socials-footer">
+                <div class="col pb-3">
+                    <a class="btn text-white btn-floating m-1" style="background-color: #3b5998;" target="_blank" href="https://www.facebook.com/DisneylandParisfr/" role="button">
+                        <i class='bx bxl-facebook' ></i>
+                    </a>
+                    <a class="btn text-white btn-floating m-1" style="background-color: #55acee;" target="_blank" href="https://twitter.com/disneylandparis" role="button">
+                        <i class='bx bxl-twitter' ></i>
+                    </a>
+                    <a class="btn text-white btn-floating m-1" style="background-color: #dd4b39;" target="_blank" href="https://www.youtube.com/user/disneylandparis" role="button">
+                        <i class='bx bxl-youtube'></i>
+                    </a>
+                    <a class="btn text-white btn-floating m-1" style="background-color: #ac2bac;" target="_blank" href="https://www.instagram.com/disneylandparis/" role="button">
+                        <i class='bx bxl-instagram' ></i>
+                    </a>
+                </div>
+                <div class="col pb-3">
+                    <a class="btn btn-floating m-1 phone-footer" href="tel:06 06 06 06 066" role="button">
+                        <i class='bx bxs-phone' ></i>
+                        <span>06 06 06 06 066</span>
+                    </a>
+                </div>
+            </div>
+    </div>
+
+    <p class="m-3 text-center text-white text-uppercase">Disneyland paris | ©UEL206 - Semaine 5</p>
+
 </footer>
+
+<link rel="stylesheet" href="assets/css/footer-styles.css">
